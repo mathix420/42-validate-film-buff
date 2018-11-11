1128d45a86ffd3df36937b3d57aa797f2807cbdc
Arnaud GISSINGER <agissing@e3r12p12.42.fr>
2018-11-11 15:22:00 +0100

    Little change

--- film_buffer.py
+++ film_buffer.py
@@ -6,7 +6,7 @@
 #    By: agissing <agissing@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/11/10 19:16:29 by agissing          #+#    #+#              #
-#    Updated: 2018/11/11 15:10:28 by agissing         ###   ########.fr        #
+#    Updated: 2018/11/11 15:21:19 by agissing         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,15 +15,16 @@ import subprocess, sys
 user_id = input("Cookie user.id=")
 session = input("Cookie _intra_42_session_production=")
 
-user_agent = '-H "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.75 Safari/537.1"'
+user_agent = '-H "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.1 (KHTML, like Gecko) Chrome/21.0.1180.75 Safari/537.1" '
+cookie = '-H "Cookie: user.id=' + user_id + '; _intra_42_session_production=' + session
 
 page_url = 'https://elearning.intra.42.fr/videos?page='
 url_list = ''
 
-print ("Please wait until all videos are loaded !")
+print ("\nPlease wait until all videos are loaded !\n")
 
 for i in range(0, 37):
-    cmd = 'curl --silent "' + page_url + str(i) + '" ' + user_agent + ' -H "Cookie: user.id=' + user_id + '; _intra_42_session_production=' + session + '" |  grep -o "/notions/[-a-zA-Z0-9@:%_\+\.~#?&\\/=]*" | sed -e "s/$/\/seen/"'
+    cmd = 'curl --silent "' + page_url + str(i) + '" ' + user_agent + cookie + '" |  grep -o "/notions/[-a-zA-Z0-9@:%_\+\.~#?&\\/=]*" | sed -e "s/$/\/seen/"'
     url_list += subprocess.check_output(['bash','-c', cmd]).decode()
 
 print ("%s videos found !" % (url_list.count('\n') - 1))
@@ -35,7 +36,7 @@ sys.stdout.write("\b" * (toolbar_width + 1))
 
 i = 0
 for url in url_list.split('\n'):
-    cmd = 'curl --silent "https://elearning.intra.42.fr' + url + '" ' + user_agent + ' -H "Cookie: user.id=' + user_id + '; _intra_42_session_production=' + session + '"'
+    cmd = 'curl --silent "https://elearning.intra.42.fr' + url + '" ' + user_agent + cookie + '"'
     out = subprocess.check_output(['bash','-c', cmd])
     if (out == b'{"seen":true}'):
         i += 1
